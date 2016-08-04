2ef27aca0a07dfdc242abc12605d0df0603b8466
James Hudson <James@hudson.gb.com>
2016-08-04 15:21:16 +0100

    Fix layout problem and h font size issue.

--- functions.php
+++ functions.php
@@ -6,18 +6,18 @@ if(class_exists("\modules\util\ClientScriptsStyles")){
 
   $styles = new \modules\util\ClientScriptsStyles();
 
-    $styles->addStyles("custom-css", get_template_directory_uri() . "-child-updated/style.css", array('aurum-main'));
+    $styles->addStyles("custom-css", get_template_directory_uri() . "-child/style.css", array('aurum-main'));
 
   $styles->addMobileStyle('custom-mobile-css',
-                          get_template_directory_uri() . "-child-updated/assets/css/mobile.css",
+                          get_template_directory_uri() . "-child/assets/css/mobile.css",
                           array('custom-css'));
 
   $styles->addDesktopStyle('custom-desktop-css',
-                           get_template_directory_uri() . "-child-updated/assets/css/desktop.css",
+                           get_template_directory_uri() . "-child/assets/css/desktop.css",
                            array('custom-css'));
 
     $styles->addDesktopStyle('custom-desktop-css',
-        get_template_directory_uri() . "-child-updated/assets/css/home.css",
+        get_template_directory_uri() . "-child/assets/css/home.css",
         array('custom-css'));
 }
 
