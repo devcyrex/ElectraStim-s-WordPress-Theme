8ca0564f3e3e1d39d355c880d36352d6c612dd7d
James Hudson <James@hudson.gb.com>
2016-08-05 14:46:12 +0100

    Fixed broking on loading svg.

--- inc/laborator_functions.php
+++ inc/laborator_functions.php
@@ -629,7 +629,7 @@ function has_transparent_header()
 
 
 # Get SVG
-function lab_get_svg_child($svg_path, $id = null, $size = array(24, 24), $is_asset = true)
+function lab_get_svg($svg_path, $id = null, $size = array(24, 24), $is_asset = true)
 {
 	if($is_asset)
 		$svg_path = get_template_directory() . '-child/assets/' .  $svg_path;
@@ -659,6 +659,41 @@ function lab_get_svg_child($svg_path, $id = null, $size = array(24, 24), $is_ass
 			'height="'.$size[0].'px"'
 		),
 		$svg
+	);
+
+	return $svg;
+}
+
+function lab_get_svg_child($svg_path, $id = null, $size = array(24, 24), $is_asset = true)
+{
+	if($is_asset)
+		$svg_path = get_template_directory() . '-child/assets/' .  $svg_path;
+
+	if( ! $id)
+		$id = sanitize_title(basename($svg_path));
+
+	if(is_numeric($size))
+		$size = array($size, $size);
+
+	ob_start();
+
+	echo file_get_contents($svg_path);
+
+	$svg = ob_get_clean();
+
+	$svg = preg_replace(
+			array(
+					'/^.*<svg/s',
+					'/id=".*?"/i',
+					'/width=".*?"/',
+					'/height=".*?"/'
+			),
+			array(
+					'<svg', 'id="'.$id.'"',
+					'width="'.$size[0].'px"',
+					'height="'.$size[0].'px"'
+			),
+			$svg
 	);
 
 	return $svg;
